0c719ae2a02023bc2378d8efe6401bf319a5d517
narminrb <narminrb@code.edu.az>
2025-03-19 04:29:18 +0400

    Add home folder inside src

---
+++ client/src/pages/home/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Banner from '../../components/sections/Banner'
+import HomeBanner from '../../components/sections/FakeBanner'
+
+const Home = () => {
+  return (
+    <div>
+      <HomeBanner/>
+    </div>
+  )
+}
+
+export default Home
