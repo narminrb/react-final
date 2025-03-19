109ba4e64024b7d3d9c0fdd8f796bf0d333f1c16
narminrb <narminrb@code.edu.az>
2025-03-20 00:53:48 +0400

    Added products swiper

--- client/src/pages/home/index.jsx
+++ client/src/pages/home/index.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Banner from '../../components/sections/Banner'
 import HomeBanner from '../../components/sections/FakeBanner'
+import Discover from '../../components/sections/Discover'
+import CustomSwiper from '../../components/sections/ProductSwiper'
+import New from '../../components/sections/New'
 
 const Home = () => {
   return (
     <div>
       <HomeBanner/>
+      <Discover/>
+      <CustomSwiper/>
+      <New/>
     </div>
   )
 }
